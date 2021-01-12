8d33834a7255125af34ffece8989e9d47ed789b6
Iso <kagiso.k.mashigo@gmial.com>
2021-01-12 20:32:24 +0000

    add authenticate with credentials

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,7 +16,7 @@ class UserController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email.downcase.strip, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  validates :email, uniqueness: { case_sensitive: false }
 
+  validates :first_name, presence: true, length: { minimum: 3}
+  validates :last_name, presence: true, length: { minimum: 3}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5}
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.downcase.strip)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+  
 end
